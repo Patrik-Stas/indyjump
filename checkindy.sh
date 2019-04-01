c74173e29e28d7c61948537c33d29060e76b751b
Patrik Stas <patrik.stas@absa.co.za>
2019-04-01 17:46:12 +0200

    Make checkindy.sh more informative

--- checkindy.sh
+++ checkindy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Library links:"
+ls -l /usr/local/lib/ | grep indy | tr -s ' ' | cut -d' ' -f 11
 
-ls -lah /usr/local/lib | grep indy
+echo " "
+echo "Dereferenced links:"
+ls -lLh /usr/local/lib/ | egrep '(libindy|libnullpay|libvcx)'
